3884a0d6d4a8b36db563d2150894412ddea5ea00
WithoutThinkAName <xu_chenhao@yahoo.com>
2018-12-10 12:04:41 +0800

    测试用代码

---
+++ Assets/DesignModeTestCode/test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using System.Linq;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       
+    }
+	
+	
+}
